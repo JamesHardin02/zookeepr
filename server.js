8d58a6d96c43e7a2bd23f5da128b73e24a140de0
JamesH <jwhusc7@gmail.com>
2022-05-07 14:51:58 -0400

    added seperate dataset code

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
+const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Use apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// parse incoming string or array including nested arrays/objs into json
-app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data in js obj
-app.use(express.json());
-// middleware for files that call dependencies load the public folder
-app.use(express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
